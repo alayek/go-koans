28cd2483675cd4d0740f53411ebc5252e9258715
alayek <arijit.layek1991@gmail.com>
2015-09-13 22:17:35 +0530

    Remove comment from the 5th line iin about_files.go to pass the test case

--- about_files.go
+++ about_files.go
@@ -3,7 +3,7 @@ package go_koans
 import "io/ioutil"
 import "strings"
 
-func aboutFiles() { // line 5
+func aboutFiles() {
 	filename := "about_files.go"
 	contents, _ := ioutil.ReadFile(filename)
 	lines := strings.Split(string(contents), "\n")
